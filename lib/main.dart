0a2d807b3e5ec7e2c12b5e4ada68414e8dad747b
Ankit Codes <hrithikrtiwari@gmail.com>
2023-03-06 22:01:52 +0530

    sizedbox

--- lib/main.dart
+++ lib/main.dart
@@ -38,52 +38,17 @@ class MyHomePage extends StatelessWidget {
     // our code goes here
     return Scaffold(
       body: Center(
-        // your code starts from here
         child: Container(
-          alignment: Alignment.topRight,
-          // color: Colors.black,
-          // height: 600,
-          decoration: BoxDecoration(
-            border:
-                Border.all(width: 2.0, color: Color.fromARGB(255, 215, 0, 0)),
-          ),
-          // width: 200,
-          padding: EdgeInsets.all(20.0),
-          // this is the child container -> orange and every children
-          child: Container(
-            // color: Colors.deepOrange,
-            decoration: BoxDecoration(
-              color: Colors.deepOrange,
-              borderRadius: BorderRadius.circular(20.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.blue,
-                  blurRadius: 20.0,
-                  spreadRadius: 20.0,
-                  offset: Offset(
-                    0,
-                    20,
-                  ),
-                ),
-              ],
-            ),
-            // height: 80,
-            // amber container
+          height: 400,
+          color: Colors.amber,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
             child: Container(
-              margin: EdgeInsets.all(20.0),
-              color: Colors.amber,
-              height: 600,
-              width: 200,
-              // padding: EdgeInsets.all(
-              //   28.0,
-              // ),
-              transform: Matrix4.rotationX(
-                Math.pi / 4,
-              ),
-              child: Image.asset(
-                // "images/flutter-banner.png",
-                "images/flutter-2.jpg",
-                fit: BoxFit.contain,
+              height: 300,
+              color: Colors.green,
+              // width: 30,
+              child: Text(
+                "Hello World",
               ),
             ),
           ),
