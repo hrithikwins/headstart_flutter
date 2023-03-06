e075cf4c6d8d070a8ebe4c56c562ec3d11fb10c6
Ankit Codes <hrithikrtiwari@gmail.com>
2023-03-06 21:23:43 +0530

    image from network in flutte

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyHomePage extends StatelessWidget {
         child: Container(
           alignment: Alignment.topRight,
           // color: Colors.black,
-          height: 600,
+          // height: 600,
           decoration: BoxDecoration(
             border:
                 Border.all(width: 2.0, color: Color.fromARGB(255, 215, 0, 0)),
@@ -59,6 +59,7 @@ class MyHomePage extends StatelessWidget {
                 BoxShadow(
                   color: Colors.blue,
                   blurRadius: 20.0,
+                  spreadRadius: 20.0,
                   offset: Offset(
                     0,
                     20,
@@ -71,17 +72,17 @@ class MyHomePage extends StatelessWidget {
             child: Container(
               margin: EdgeInsets.all(20.0),
               color: Colors.amber,
-              height: 80,
+              height: 600,
               width: 200,
-              padding: EdgeInsets.all(
-                28.0,
-              ),
-              // transform: Matrix4.rotationX(
-              //   Math.pi,
+              // padding: EdgeInsets.all(
+              //   28.0,
               // ),
-              child: Text(
-                "Hello World ",
-
+              transform: Matrix4.rotationX(
+                Math.pi / 4,
+              ),
+              child: Image.network(
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fih1.redbubble.net%2Fimage.1187851051.8476%2Fflat%2C750x%2C075%2Cf-pad%2C750x1000%2Cf8f8f8.jpg&f=1&nofb=1&ipt=2ef09687efba91b6cfe5a6da2a8cddd5e506c06615fd1bbe9c97d6da7eb7f90f&ipo=images",
+                fit: BoxFit.cover,
               ),
             ),
           ),
