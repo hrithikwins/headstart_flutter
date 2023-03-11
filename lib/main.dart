b3818e6d071f5b7687fb2fd231e0ba2c92c942c4
Ankit Codes <hrithikrtiwari@gmail.com>
2023-03-11 21:28:44 +0530

    column properties

--- lib/main.dart
+++ lib/main.dart
@@ -43,20 +43,37 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // our code goes here
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 400,
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Container(
-              height: 300,
-              // width: 30,
-              child: Image.asset(
-                images["image2"].toString(),
-                // "https://s3-alpha-sig.figma.com/img/3e9c/837e/5696c17cab030c3aa1de6a8374e44294?Expires=1679270400&Signature=PFNumXxEHrKlPML9sxZNf-WP2JRbH9YEz801lb1bW4LXngN3i~izC~1zLzUg4wMQsUh5b35oALhrsx1syE3cUHuU1VhJfOMwTDlvOLDWiNm84F17vLf8SvPsQ8cxqGAHNn9jvU~ojyBrH6VbFOiSiJkd9Ka9wRroor13LPSWA0hD-Z8kSFDtI0Ucel5j6~eKK3vM7~gBRXp65QnnC-hSByYKd-SLVDL~nMyfUXv1ozlvyU5U0RYJOmA~Ari2uxsE6ftRvBGHPbyrNmfY2PRYLLUM255lqZHjOQ~XeAwODD11rn2wrRZycPVX77lV9P0NdJvm-M78WggFogQ7bUlDRQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-              ),
+      //top to bottom(vertical axis) -> mainAxis
+      //l horizontal axis => cross Axis of column
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // textDirection: TextDirection.rtl,
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              height: 120,
+              width: 300,
+              color: Colors.green,
+              child: Text("Hello"),
             ),
-          ),
+            Container(
+              height: 120,
+              width: 300,
+              color: Colors.green,
+              child: Text("Hello"),
+            ),
+
+            Container(
+              height: 120,
+              width: 300,
+              color: Colors.green,
+              child: Text("Hello"),
+            ),
+          ],
         ),
       ),
     );
