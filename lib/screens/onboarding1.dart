8998509caab8a9b5e5d900a3e5f8b1de4cc217be
Ankit Codes <hrithikrtiwari@gmail.com>
2023-03-13 21:33:02 +0530

    nwe changes

--- lib/screens/onboarding1.dart
+++ lib/screens/onboarding1.dart
@@ -46,6 +46,7 @@ class MyHomePage extends StatelessWidget {
               height: 300,
               // width: 30,
               child: Image.network(
+
                 "https://s3-alpha-sig.figma.com/img/3e9c/837e/5696c17cab030c3aa1de6a8374e44294?Expires=1679270400&Signature=PFNumXxEHrKlPML9sxZNf-WP2JRbH9YEz801lb1bW4LXngN3i~izC~1zLzUg4wMQsUh5b35oALhrsx1syE3cUHuU1VhJfOMwTDlvOLDWiNm84F17vLf8SvPsQ8cxqGAHNn9jvU~ojyBrH6VbFOiSiJkd9Ka9wRroor13LPSWA0hD-Z8kSFDtI0Ucel5j6~eKK3vM7~gBRXp65QnnC-hSByYKd-SLVDL~nMyfUXv1ozlvyU5U0RYJOmA~Ari2uxsE6ftRvBGHPbyrNmfY2PRYLLUM255lqZHjOQ~XeAwODD11rn2wrRZycPVX77lV9P0NdJvm-M78WggFogQ7bUlDRQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
               ),
             ),
