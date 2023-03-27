6e5ce043c15238ab20045b15b2ab6bd125cabdda
Ankit Codes <hrithikrtiwari@gmail.com>
2023-03-27 21:58:36 +0530

    fireestore utils created

---
+++ lib/utils/firebase-utils.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreUtils {
+  static FirebaseFirestore db = FirebaseFirestore.instance;
+
+//create
+  static Future<void> addDataToCollection(
+      String collectionName, Map<String, dynamic> data) async {
+    await db.collection(collectionName).add(data);
+  }
+
+  //read
+  static Future<DocumentSnapshot> getDataFromCollection(
+      String collectionName, String docId) async {
+    return await db.collection(collectionName).doc(docId).get();
+  }
+
+//read all
+  static Future<QuerySnapshot> getAllDataFromCollection(
+      String collectionName) async {
+    return await db.collection(collectionName).get();
+  }
+
+//update
+  static Future<void> updateDataToCollection(
+      String collectionName, String docId, Map<String, dynamic> data) async {
+    await db.collection(collectionName).doc(docId).update(data);
+  }
+
+  //delete
+  static Future<void> deleteDataFromCollection(
+      String collectionName, String docId) async {
+    await db.collection(collectionName).doc(docId).delete();
+  }
+}
